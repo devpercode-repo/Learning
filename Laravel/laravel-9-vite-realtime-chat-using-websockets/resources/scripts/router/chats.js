3fb1c772cdb482ff89cab0a17b52d089adb4305a
DevperCode <isxxcode@gmail.com>
2022-07-02 17:06:47 -0700

    first commit

--- Laravel/laravel-9-vite-realtime-chat-using-websockets/resources/scripts/router/chats.js
+++
@@ -1,9 +0,0 @@
-const chats = [
-      {
-        path: '/console/chat/:id',
-        name: 'chat',
-        component: () => import('../views/chats/ChatView.vue')
-      },
-    ];
-  
-  export default chats
